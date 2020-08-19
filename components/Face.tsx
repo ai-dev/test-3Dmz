6564c0c10025975bcdfb45db5ac93a2c3094151e
angelo <iasevoli90@gmail.com>
2020-08-19 17:01:02 +0200

    Add activity indicator on avatar loading

--- components/Face.tsx
+++ components/Face.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native'
 
 import AppTypes from './../types'
 
@@ -8,14 +8,20 @@ type Props = {
 }
 
 const Face: React.FC<Props> = ({ data }) => {
+  const [showLoader, setShowLoader] = useState<boolean>(true)
+
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.avatar}
-        source={{
-          uri: data.avatar,
-        }}
-      />
+      <View style={styles.avatarContainer}>
+        {showLoader && <ActivityIndicator size={50} />}
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: data.avatar,
+          }}
+          onLoadEnd={() => setShowLoader(false)}
+        />
+      </View>
       <View style={styles.nameContainer}>
         <Text style={styles.nameText}>{data.name}</Text>
       </View>
@@ -33,11 +39,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderRadius: 100,
   },
-  avatar: {
+  avatarContainer: {
     width: 50,
     height: 50,
     backgroundColor: '#fff',
     borderRadius: 25,
+    overflow: 'hidden',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
   },
   nameContainer: {
     justifyContent: 'center',
